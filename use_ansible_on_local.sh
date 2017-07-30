4db531f952138bc06696f8c11bdae7053afeb5a3
timle1 <boston1re@gmail.com>
2017-07-30 16:41:35 -0400

    add install docker to setup

--- use_ansible_on_local.sh
+++ use_ansible_on_local.sh
@@ -1,4 +1,5 @@
 # look for ansible, then install ansible if not installed
 sudo ./install_ansible.sh
 # use ansible
-ansible-playbook -i inventory.ini main.yml --ask-become-pass --extra-vars "skip_dotfiles=false skip_docker_machine=false"
+ansible-playbook -i inventory.ini main.yml --ask-become-pass --extra-vars "skip_dotfiles=false"
+ansible-playbook -i inventory.ini install_docker_machine.yml --ask-become-pass --extra-vars "skip_docker_machine=false"
